0fd06ce0fcef84e86d38e9336776f21283ce4657
mikelangston <mikemac8203@gmail.com>
2015-02-06 11:00:03 -0500

    added paperclip

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -70,7 +70,7 @@ class BooksController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def book_params
-      params.require(:book).permit(:name, :author, :description, :price, :availability)
+      params.require(:book).permit(:name, :author, :description, :price, :availability, :image, :resource)
     end
 
     def authorize_user
